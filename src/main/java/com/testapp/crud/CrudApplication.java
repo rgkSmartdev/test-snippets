4cecdcf7f91963a9b7ae5f035622b2d1d99beace
smartdev <rgk.smartdev@outlook.com>
2024-12-24 00:53:34 +0530

    added commanLineRunner to load all beans in context

--- src/main/java/com/testapp/crud/CrudApplication.java
+++ src/main/java/com/testapp/crud/CrudApplication.java
@@ -1,7 +1,12 @@
 package com.testapp.crud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -10,4 +15,15 @@ public class CrudApplication {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Lets inspect beans provided by spring boot!");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
---
+++ src/main/java/com/testapp/crud/HelloController.java
@@ -0,0 +1,13 @@
+package com.testapp.crud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World! from Crud app!";
+    }
+}
